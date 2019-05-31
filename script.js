9ed007f7cc5fe51a792e65848c2ba6393871c652
want74 <33675774+want74@users.noreply.github.com>
2019-05-31 14:26:55 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let rightOne = document.querySelector('.rightOne')
+let rightTwo = document.querySelector('.rightOne')
+let rightThree = document.querySelector('.rightOne')
+let rightFour = document.querySelector('.rightOne')
+rightOne.onclick = function() 
+{
+	rightOne.style.display = 'none';
+	rightTwo.style.display = 'block';
+}
